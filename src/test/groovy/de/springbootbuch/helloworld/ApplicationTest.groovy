8d19ff3db657ad80bd3952b5261258ab03103318
Gerrit Meier <meistermeier@gmail.com>
2020-11-02 20:44:20 +0100

    Upgrade to Spring Boot 2.4.0-RC1.

--- src/test/groovy/de/springbootbuch/helloworld/ApplicationTest.groovy
+++ src/test/groovy/de/springbootbuch/helloworld/ApplicationTest.groovy
@@ -1,10 +1,11 @@
 package de.springbootbuch.helloworld
 
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
